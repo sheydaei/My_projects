7c0ed35a0ebdbf0e2197ad1245f403197c59ff07
Sheyda <sheyda.sheydayi95@gmail.com>
2024-07-26 13:20:15 +0200

    move spiced

---
+++ Spiced/adventure.py
@@ -0,0 +1,12 @@
+print("""
+Find the Dragon Egg
+===================
+fghgfYourgffjh quest startdfsds.
+""")
+
+
+print("""
+On a hidden clearing you discover the dragon egg.
+
+Your quest has been successful!
+""")
