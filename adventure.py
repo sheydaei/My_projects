95bb7673a4328f869d812cc47ed33b42e261fb9c
Sheyda <sheyda.sheydayi95@gmail.com>
2024-07-26 12:07:59 +0200

    Initial commit

---
+++ adventure.py
@@ -0,0 +1,13 @@
+print("""
+Find the Dragon Egg
+===================
+
+Your quest starts.
+""")
+
+
+print("""
+On a hidden clearing you discover the dragon egg.
+
+Your quest has been successful!
+""")
